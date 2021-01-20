a54193eeb316b1f69b6a750356228149cff124c7
Thilin <fernando_cauper@hotmail.com>
2021-01-19 20:27:20 -0400

    first commit

---
+++ src/main/java/com/mxh/wotvpvpstats/WotvpvpstatsApplication.java
@@ -0,0 +1,13 @@
+package com.mxh.wotvpvpstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WotvpvpstatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WotvpvpstatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mxh/wotvpvpstats/WotvpvpstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mxh.wotvpvpstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WotvpvpstatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
